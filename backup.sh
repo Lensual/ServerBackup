bc2d269c3fe59abecc4120c57b814c1e43e727b2
Lensual <lenvisual@gmail.com>
2019-01-21 05:28:15 +0800

    添加优先级支持

--- backup.sh
+++ backup.sh
@@ -11,9 +11,10 @@ function cleanArgs() {
     excludeList=""
     option=""
     basename=""
+    nice=""
 }
 
-workdir=/root/script/backup
+workdir=/root/script/ServerBackup
 logDir=$workdir/logs
 
 configs=$(ls $workdir"/conf/"*.conf)
@@ -25,7 +26,7 @@ do
     basename=$(basename $conf)
     filename=${basename%%.*}
 
-    for e in ${excludeList[@]}i
+    for e in ${excludeList[@]}
     do
         exclude="$exclude --exclude '$e'"
     done
@@ -36,10 +37,10 @@ do
 	#TODO 并行优化
 	
 	#奇怪问题：如果不使用bash单独运行rsync，在脚本中执行会忽略掉排除列表
-    bash -c "rsync $option --delete --bwlimit=${bwlimit} -e \"ssh -p ${port} -i ${sshKey} -o 'StrictHostKeyChecking no'\" ${exclude} ${user}@${host}:${srcDir} ${backupDir}/${filename}/ | tee ${logDir}/${filename}_$timestamp.log"
+    nice -n ${nice} bash -c "rsync $option --delete --bwlimit=${bwlimit} -e \"ssh -p ${port} -i ${sshKey} -o 'StrictHostKeyChecking no'\" ${exclude} ${user}@${host}:${srcDir} ${backupDir}/${filename}/ | tee ${logDir}/${filename}_$timestamp.log"
 
 	#TODO 自动检测cpu核心数
 	#TODO 参数使用变量配置
-    tar cpf - ${backupDir}/${filename}/ | 7za a -t7z -m0=lzma2 -mx9 -mmt8 -si ${backupDir}/${filename}_$timestamp.7z
+    nice -n ${nice} bash -c "tar cpf - ${backupDir}/${filename}/ | 7za a -t7z -m0=lzma2 -mx9 -mmt8 -si ${backupDir}/${filename}_$timestamp.7z"
 done
 
