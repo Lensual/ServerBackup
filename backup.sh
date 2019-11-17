72262c4ac4affad797c72a7591c43fd1643ab0d6
神楽坂ニャン <lenvisual@gamil.com>
2019-11-18 04:41:07 +0800

    添加注释 添加优先级选项 自动使用全部cpu核心 可以使用多种压缩方式

--- backup.sh
+++ backup.sh
@@ -14,8 +14,15 @@ function cleanArgs() {
     nice=""
 }
 
-workdir=/root/script/ServerBackup
-logDir=$workdir/logs
+workdir=$(dirname $0)	#工作路径
+logDir=$workdir/logs	#日志路径
+mkdir -p $logDir
+cpuNum=$(nproc)		#cpu数量
+
+compType=7z		#压缩格式 还可以用xz gz等
+p7z_mx=9		#7z压缩质量
+p7z_m0=lzma2		#7z压缩算法
+
 
 configs=$(ls $workdir"/conf/"*.conf)
 for conf in $configs
@@ -23,6 +30,7 @@ do
     cleanArgs
     source $conf
 
+    #获取配置文件的名字，用这个名字存档
     basename=$(basename $conf)
     filename=${basename%%.*}
 
@@ -33,14 +41,29 @@ do
 
     timestamp=$(date +%Y-%m-%d_%T)
 
-	#TODO 改变优先级
-	#TODO 并行优化
+    #TODO 并行优化
 	
-	#奇怪问题：如果不使用bash单独运行rsync，在脚本中执行会忽略掉排除列表
+    #奇怪问题：如果不使用bash单独运行rsync，在脚本中执行会忽略掉排除列表
     nice -n ${nice} bash -c "rsync $option --delete --bwlimit=${bwlimit} -e \"ssh -p ${port} -i ${sshKey} -o 'StrictHostKeyChecking no'\" ${exclude} ${user}@${host}:${srcDir} ${backupDir}/${filename}/ | tee ${logDir}/${filename}_$timestamp.log"
 
-	#TODO 自动检测cpu核心数
-	#TODO 参数使用变量配置
-    nice -n ${nice} bash -c "tar cpf - ${backupDir}/${filename}/ | 7za a -t7z -m0=lzma2 -mx9 -mmt8 -si ${backupDir}/${filename}_$timestamp.7z"
+    if [ $compType == "7z" ]; then
+        nice -n ${nice} bash -c "tar cpf - ${backupDir}/${filename}/ | 7za a -t7z -m0=${p7z_m0} -mx${p7z_mx} -mmt${cpuNum} -si ${backupDir}/${filename}_$timestamp.7z"
+    elif [ $compType == "xz" ]; then
+        nice -n ${nice} bash -c "tar Jcpf ${backupDir}/${filename}_$timestamp.tar.xz ${backupDir}/${filename}/"
+    elif [ $compType == "gz" ]; then
+        nice -n ${nice} bash -c "tar zcpf ${backupDir}/${filename}_$timestamp.tar.gz ${backupDir}/${filename}/"
+    elif [ $compType == "bz" ]; then
+        nice -n ${nice} bash -c "tar jcpf ${backupDir}/${filename}_$timestamp.tar.bz ${backupDir}/${filename}/"
+    elif [ $compType == "bz2" ]; then
+        nice -n ${nice} bash -c "tar jcpf ${backupDir}/${filename}_$timestamp.tar.bz2 ${backupDir}/${filename}/"
+    elif [ $compType == "z" ]; then
+        nice -n ${nice} bash -c "tar Zcpf ${backupDir}/${filename}_$timestamp.tar.z ${backupDir}/${filename}/"
+    elif [ $compType == "zip" ]; then
+        nice -n ${nice} bash -c "zip -qrSV ${backupDir}/${filename}_$timestamp.zip ${backupDir}/${filename}/"
+    elif [ $compType == "rar" ]; then
+        nice -n ${nice} bash -c "rar a -r ${backupDir}/${filename}_$timestamp.rar ${backupDir}/${filename}/"
+    elif [ $compType == "tar" ]; then
+        nice -n ${nice} bash -c "tar cpf ${backupDir}/${filename}_$timestamp.tar ${backupDir}/${filename}/"
+    fi
 done
 
